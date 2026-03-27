0b524f9a38046221add6c1d1de92ad5b64cf8278
chhatrodiya ravi <trendcart04@gmail.com>
2026-03-27 14:17:56 +0530

    created admin ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import AdminLayout from "./components/layout/AdminLayout";
 
 // Admin Pages
-import AdminDashboard from './pages/admin/Dashboard';
-import EventManagement from './pages/admin/EventManagement';
-import ParticipantManagement from './pages/admin/ParticipantManagement';
-import JudgeManagement from './pages/admin/JudgeManagement';
-import PaymentManagement from './pages/admin/PaymentManagement';
-import Scoring from './pages/admin/Scoring';
+import AdminDashboard from "./pages/admin/Dashboard";
+import EventManagement from "./pages/admin/EventManagement";
+import ParticipantManagement from "./pages/admin/ParticipantManagement";
+import JudgeManagement from "./pages/admin/JudgeManagement";
+import PaymentManagement from "./pages/admin/PaymentManagement";
+import Scoring from "./pages/admin/Scoring";
+import ScoringOversight from "./pages/admin/ScoringOversight";
 
 // Event Pages
-import Home from './pages/event/Home';
-import EventDetails from './pages/event/EventDetails';
-import Leaderboard from './pages/event/Leaderboard';
+import Home from "./pages/event/Home";
+import EventDetails from "./pages/event/EventDetails";
+import Leaderboard from "./pages/event/Leaderboard";
 
 // Team Pages
-import Directory from './pages/team/Directory';
-import Registration from './pages/team/Registration';
+import Directory from "./pages/team/Directory";
+import Registration from "./pages/team/Registration";
+import TeamsDirectory from "./pages/team/TeamsDirectory";
 
 // Judge Pages
-import JudgeDashboard from './pages/judge/Dashboard';
+import JudgeDashboard from "./pages/judge/Dashboard";
 
 function App() {
   return (
@@ -28,21 +36,26 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/events/:id" element={<EventDetails />} />
         <Route path="/events/:id/leaderboard" element={<Leaderboard />} />
-        
+
         {/* Team Routes */}
         <Route path="/teams" element={<Directory />} />
         <Route path="/register" element={<Registration />} />
-        
+
         {/* Judge Routes */}
         <Route path="/judge/dashboard" element={<JudgeDashboard />} />
 
         {/* Admin Routes */}
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        <Route path="/admin/events" element={<EventManagement />} />
-        <Route path="/admin/participants" element={<ParticipantManagement />} />
-        <Route path="/admin/judges" element={<JudgeManagement />} />
-        <Route path="/admin/payments" element={<PaymentManagement />} />
-        <Route path="/admin/scoring" element={<Scoring />} />
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Navigate to="dashboard" replace />} />
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="events" element={<EventManagement />} />
+          <Route path="teams" element={<TeamsDirectory />} />
+          <Route path="participants" element={<ParticipantManagement />} />
+          <Route path="judges" element={<JudgeManagement />} />
+          <Route path="payments" element={<PaymentManagement />} />
+          <Route path="scoring" element={<Scoring />} />
+          <Route path="reports" element={<ScoringOversight />} />
+        </Route>
       </Routes>
     </Router>
   );
